0a6eb609853e18a143f63ce455f774d93075be28
Wayne <wpa12@live.com>
2026-05-04 14:57:59 +0100

    tweaks

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\LessonSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -30,6 +31,7 @@ class DatabaseSeeder extends Seeder
             AircraftSeeder::class,
             ExamSeeder::class,
             InstructorSeeder::class,
+            LessonSeeder::class,
             BookingSeeder::class,
         ]);
     }
