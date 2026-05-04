9bb0c60bae882ad5e752649a71dda0047d80cdf8
Wayne <wpa12@live.com>
2026-05-04 11:23:21 +0100

    some addtional small changes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', [PageController::class, 'index']);
 
 Route::get('/aircraft', [PageController::class, 'aircraft'])->name('aircraft');
 
+Route::get('/exams', [PageController::class, 'exams'])->name('exams');
+
+Route::get('/instructors', [PageController::class, 'instructors'])->name('instructors');
+
 Route::get('/login', [PageController::class, 'login']);
 
 Route::get('/register', [PageController::class, 'register']);
