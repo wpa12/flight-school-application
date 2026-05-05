c25491408dc8f5d53ab8b72ee2a7b587415bf4ae
Wayne <wpa12@live.com>
2026-05-05 14:48:02 +0100

    notifications added for Booking cancelled, confirmed, updated, and reminder

---
+++ app/Notifications/BookingReminderNotification.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\Booking;
+
+class BookingReminderNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(private Booking $booking)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->greeting('Hello ' . ($notifiable->name ?? 'there') . ',')
+            ->line('Reminder that your booking '. $this->booking->bookableSummary() . 'at' . $this->booking->booking_date_time_start->format('M j, Y g:i A') . ' is coming up soon.')
+            ->action('View your bookings on the dashboard', route('dashboard.index'))
+            ->line('Thank you for using Bristol Flight School!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ app/Notifications/BookingUpdatedNotification.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\Booking;
+
+class BookingUpdatedNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(private Booking $booking)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)    
+            ->greeting('Hello ' . ($notifiable->name ?? 'there') . ',')
+            ->line('Your booking '. $this->booking->bookableSummary() . 'has been updated.')
+            ->action('View your bookings on the dashboard', route('dashboard.index'))
+            ->line('Thank you for using Bristol Flight School!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
